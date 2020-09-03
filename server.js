119b555b761a2ab89c6347e703da434e87d43083
Jon Penn <jpenn@bentonvillek12.org>
2020-09-02 20:04:41 -0500

    add server code

---
+++ server.js
@@ -0,0 +1,65 @@
+// Put the following global function to your project.
+function repl(__tunnelURL) {
+  function inspect(object) {
+    return Object.keys(object).sort().map(
+      (function(k){
+        return typeof object[k] + " [" + k + "] = " + object[k];
+      }));
+  }
+  
+  let __value = '__READY';
+  do {
+    Logger.log(`${__value}`);
+    
+    const __response = UrlFetchApp.fetch(
+      __tunnelURL,
+      {
+        'method': 'post',
+        'contentType': 'text/plain',
+        'payload': __value
+      }
+    ).getContentText();
+    
+    Logger.log(`> ${__response}`);
+    
+    if (__response == "__USER_INPUT_TIMEOUT") {
+      __value = '__KEEPALIVE';
+      continue;
+    }
+    
+    if (__response == "__DISCONNECT") {
+      break;
+    }
+    
+    if (/^__DESCRIBE .+$/.test(__response)) {
+      try {
+        let __varName = __response.match(/^__DESCRIBE (.+)$/)[1];
+        const __propNames = Object.getOwnPropertyNames(eval(__varName));
+        const __properties = __propNames.map(__propName => (
+          {
+            name: __propName,
+            type: typeof eval(__varName)[__propName]
+          }
+        ));
+        __value = JSON.stringify(__properties);
+      } catch(__error) {
+        Logger.log(__error);
+        __value = '[]';
+      }
+      continue;
+    }
+    
+    try {
+      __value = JSON.stringify(eval(__response), null, 2);
+    } catch (__error) {
+      Logger.log(__error);
+      __value = __error
+    }
+    
+  } while (true);
+}
+
+
+function start() {
+  repl('http://example.com');
+}
